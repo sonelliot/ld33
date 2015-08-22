88586957e6e9f4936e62e9356f633b637f915735
Elliot Bulmer <sonelliot@gmail.com>
2015-08-22 20:32:48 +0800

    Move camera with middle mouse button

--- src/cameraman.js
+++ src/cameraman.js
@@ -15,5 +15,15 @@ export class CameraMan {
       this.camera.x -= 4;
     else if (this.cursors.right.isDown)
       this.camera.x += 4;
+
+    let pointer = this.game.input.activePointer;
+    if (pointer.middleButton.isDown) {
+      let {width, height} = this.game;
+      let center = new Phaser.Point(width / 2, height / 2);
+      let direction = Phaser.Point.subtract(pointer.position, center);
+      direction.normalize();
+      this.camera.x += direction.x * 4;
+      this.camera.y += direction.y * 4;
+    }
   }
 };
