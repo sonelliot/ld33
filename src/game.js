46c36ccda6c60d42e4deb302a20f70d2b8d1474d
Elliot Bulmer <sonelliot@gmail.com>
2015-08-22 16:40:50 +0800

    Back to the beginning

--- src/game.js
+++ src/game.js
@@ -1,12 +1,7 @@
 // Main entry point of the game.
 
-import {Entity} from './entity.js';
-import {System} from './system.js';
-
 let game = new Phaser.Game(800, 600, Phaser.AUTO, null, {
   preload, create, update });
-
-let map = null;
 
 function preload(game) {
   function sprite(name) {
@@ -37,25 +32,9 @@ function preload(game) {
 function create(game) {
   game.stage.backgroundColor = 0x363636;
   game.physics.startSystem(Phaser.Physics.ARCADE);
-
-  let entities = [], systems = [];
-
-  systems.push(System.display(game));
-  systems.push(System.selected(game));
-  systems.push(System.pulsate(game));
-
-  entities.push(Entity.level(game, 'test', [
-    'floor_tile', 'box_tile', 'tileset_wall' ]));
-  entities.push(Entity.disk(game));
-  entities.push(Entity.guard(game));
-
-  game.entities = entities;
-  game.systems = systems;
 }
 
 function update(game) {
-  game.entities = game.entities.map(
-    entity => game.systems.reduce((ent, sys) => sys.apply(ent), entity));
 }
 
 window.game = game;
