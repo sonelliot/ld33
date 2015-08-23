3a29bc440abc67191cab492427ad7f3a65062fdd
Elliot Bulmer <sonelliot@gmail.com>
2015-08-23 09:01:55 +0800

    Improve pathfinding a bit

--- src/guard.js
+++ src/guard.js
@@ -4,14 +4,15 @@ export class Guard {
     this.selected = false;
     this.destination = null;
     this.speed = 100.0;
+    this.path = [];
 
     this.sprite = {};
     this.group = game.add.group();
 
     this.sprite.guard = game.add.sprite(0, 0, 'guard', null, this.group);
     this.sprite.guard.position.set(position.x, position.y);
-    this.sprite.guard.anchor.set(0.5, 0.5);
-    this.sprite.guard.scale.set(3, 3);
+    this.sprite.guard.scale.set(3,3);
+    this.sprite.guard.anchor.set(0.5, 1.0);
     this.sprite.guard.smoothed = false;
     this.sprite.guard.inputEnabled = true;
 
@@ -21,17 +22,42 @@ export class Guard {
 
     game.physics.enable(this.sprite.guard, Phaser.Physics.ARCADE);
     this.sprite.guard.body.collideWorldBounds = true;
+    this.sprite.guard.body.setSize(5,5,0,0);
 
     let pointer = game.input.activePointer;
     pointer.leftButton.onDown.add(_ => {
       if (this.selected === true) {
-        this.destination =
-          new Phaser.Point(pointer.worldX, pointer.worldY);
+        let level = this.game.level;
+        let sprite = this.sprite.guard;
+        let target = new Phaser.Point(pointer.worldX, pointer.worldY);
+        level.path(sprite.position, target, path => {
+          this.path = path.splice(1);
+          this.showPath(this.path);
+        });
       }
     });
+
+    this.disks = [];
+    for (let i = 0; i < 100; i++) {
+      let d = game.add.sprite(0, 0, 'disk');
+      d.anchor.set(0.5, 0.5);
+      d.scale.set(2,2);
+      d.visible = false;
+      this.disks.push(d);
+    }
   }
 
-  select() {
+  showPath(path) {
+    this.disks.forEach(d => d.visible = false);
+    for (let i =0; i < path.length; i++) {
+      let point = path[i];
+      let disk = this.disks[i];
+      disk.position.set(point.x, point.y);
+      disk.visible = true;
+    }
+  }
+
+  select(target) {
     this.stop();
     if (this.selected === true) {
       this.selected = false;
@@ -43,21 +69,29 @@ export class Guard {
     }
   }
 
-  update() {
-    if (this.destination !== null) {
-      let sprite = this.sprite.guard;
-      let between = Phaser.Point.subtract(this.destination, sprite.position);
-      let dirn = Phaser.Point.normalize(between);
-      sprite.body.velocity.set(this.speed * dirn.x, this.speed * dirn.y);
+  move(dest) {
+    let sprite = this.sprite.guard;
+    let between = Phaser.Point.subtract(dest, sprite.position);
+    let dirn = Phaser.Point.normalize(between);
+    sprite.body.velocity.set(this.speed * dirn.x, this.speed * dirn.y);
+    if (between.getMagnitude() < 1.0)
+      return true;
+    return false;
+  }
 
-      if (between.getMagnitude() < 1.0) {
-        this.stop();
-      }
+  update() {
+    this.game.debug.body(this.sprite.guard);
+
+    if (this.path.length > 0) {
+      let next = this.path[0];
+      if (this.move(next))
+        this.path = this.path.splice(1);
     }
+    else
+      this.stop();
   }
 
   stop() {
-    this.destination = null;
     this.sprite.guard.body.velocity.set(0,0);
   }
 };
--- src/level.js
+++ src/level.js
@@ -1,3 +1,4 @@
+import * as EasyStar from 'easystarjs';
 
 export class Level {
   constructor(game, name, tilesets) {
@@ -7,15 +8,57 @@ export class Level {
     for (let tileset of tilesets)
       this.tilemap.addTilesetImage(tileset, tileset);
 
-    this.ground = this.tilemap.createLayer('ground');
-    this.ground.smoothed = false;
-    this.ground.setScale(3,3);
-
-    this.blocked = this.tilemap.createLayer('blocked');
-    this.blocked.smoothed = false;
-    this.blocked.setScale(3,3);
+    for (let name of ['ground', 'blocked', 'walls']) {
+      let layer = this.tilemap.createLayer(name);
+      layer.smoothed = false;
+      layer.setScale(3,3);
+      this[name] = layer;
+    }
 
     this.tilemap.setCollisionBetween(1, 100000, true, this.blocked, true);
     this.ground.resizeWorld();
+
+    this.walkable = Level.walkable(this.tilemap, this.blocked);
+    this.pathfinder = new EasyStar.js();
+    this.pathfinder.setGrid(this.walkable);
+    this.pathfinder.setAcceptableTiles([0]);
+    this.pathfinder.enableDiagonals();
+    // this.pathfinder.enableCornerCutting();
+  }
+
+  path(start, end, then) {
+    let scale = { x: 3, y: 3 };
+    let startIndex = Level.tileIndex(start, this.tilemap, scale);
+    let endIndex = Level.tileIndex(end, this.tilemap, scale);
+    this.pathfinder.findPath(startIndex.x, startIndex.y, endIndex.x, endIndex.y, path => {
+      if (path === null) return then(path);
+      let width = this.tilemap.tileWidth * scale.x;
+      let height = this.tilemap.tileHeight * scale.y;
+      return then(path.map(point => new Phaser.Point(
+        (point.x) * width + (width / 2),
+        (point.y) * height + (height / 2))));
+    });
+    this.pathfinder.calculate();
+  }
+
+  static tileIndex(point, tilemap, scale) {
+    const {tileWidth, tileHeight} = tilemap;
+    return {
+      x: Math.floor(point.x / (tileWidth * scale.x)),
+      y: Math.floor(point.y / (tileHeight * scale.y))
+    };
+  }
+
+  static walkable(tilemap, layer) {
+    let tiles = [];
+    for (let y = 0; y < tilemap.width; y++) {
+      let row = [];
+      for (let x = 0; x < tilemap.height; x++) {
+        let tile = tilemap.getTile(x, y, layer);
+        row.push((tile === null) ? 0 : 1);
+      }
+      tiles.push(row);
+    }
+    return tiles;
   }
 };
